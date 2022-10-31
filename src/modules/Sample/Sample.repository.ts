ce19b099027df813fbd6d76c5ff175e3e47d78f8
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-10-31 14:17:34 +0100

    complete example files

--- src/modules/Sample/Sample.repository.ts
+++ src/modules/Sample/Sample.repository.ts
@@ -3,4 +3,33 @@ import SampleDTO from "./Sample.dto";
 export default class SampleRepository implements BaseRepository<SampleDTO> {
   // Entity manager should go here
   constructor() {}
+
+  create(data: Partial<SampleDTO>): Promise<SampleDTO> {
+    throw new Error("Method not implemented.");
+  }
+  createMany(data: Partial<SampleDTO>[]): Promise<SampleDTO[]> {
+    throw new Error("Method not implemented.");
+  }
+  update(id: number, data: Partial<SampleDTO>): Promise<SampleDTO> {
+    throw new Error("Method not implemented.");
+  }
+  delete(id: number): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  getCount(): Promise<number> {
+    throw new Error("Method not implemented.");
+  }
+
+  findAll(
+    limit: number,
+    skip: number,
+    rest?: Record<string, any>
+  ): Promise<TResultService<SampleDTO>> {
+    throw new Error("Method not implemented.");
+  }
+
+  findById(id: number): Promise<SampleDTO> {
+    throw new Error("Method not implemented.");
+  }
 }
